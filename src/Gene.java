15e074203656933fee2cf64b22300f749cac2a22
ChrisWuUNM <cwu@unm.edu>
2017-02-24 12:56:58 -0700

    IntelliJ files
    Trying to get this to work on the Linux machines in the Lab

--- src/Gene.java
+++ src/Gene.java
@@ -1,40 +1,54 @@
 /**
  * Created by c on 2/23/2017.
- *  Contains
+ *  Gene properties.
  */
 public class Gene
 {
+    private int[] gene;
+
+    Gene()
+    {
+        gene = new int[]{0,0,0,0,0,0};
+    }
+
     public enum Instruction{
-        DAT(2),            // data (kills the process)
-        MOV(2),            // move (copies data from one address to another)
-        ADD(2),            // add (adds one number to another)
-        SUB(2),            // subtract (subtracts one number from another)
-        MUL(2),            // multiply (multiplies one number with another)
-        DIV(2),            // divide (divides one number with another)
-        MOD(2),            // modulus (divides one number with another and gives the remainder)
-        JMP(2),            // jump (continues execution from another address)
-        JMZ(2),            // jump if zero (continues execution from another address)
-        JMN(2),            // jump if not zero (tests a number and jumps if it isn't 0)
-        DJN(2),            // decrement and jump if not zero (decrements a number by one and jumps unless the result is 0)
-        SPL(2),            // split (starts an execution thread at another address)
-        CMP(2),            // compare (same as SEQ)
-        SEQ(2),            // skip if equal (compares two instructions, and skips the next instruction if they are equal)
-        SNE(2),            // skip if not equal (compares two instructions, and skips the next instruction if they aren't equal)
-        SLT(2),            // skip if lower than (compares two values, and skips the next instruction if the first is lower than the second)
-        LDP(2),            // load from p-space (loads a number from private storage space)
-        STP(2),            // save to p-space (saves a number to private storage space)
-        NOP(0);             // no operation (does nothing)
+        DAT("DAT",2),           // data (kills the process)
+        MOV("MOV",2),           // move (copies data from one address to another)
+        ADD("ADD",2),           // add (adds one number to another)
+        SUB("SUB",2),           // subtract (subtracts one number from another)
+        MUL("MUL",2),           // multiply (multiplies one number with another)
+        DIV("DIV",2),           // divide (divides one number with another)
+        MOD("MOD",2),           // modulus (divides one number with another and gives the remainder)
+        JMP("JMP",2),           // jump (continues execution from another address)
+        JMZ("JMZ",2),           // jump if zero (continues execution from another address)
+        JMN("JMN",2),           // jump if not zero (tests a number and jumps if it isn't 0)
+        DJN("DJN",2),           // decrement and jump if not zero (decrements a number by one and jumps unless the result is 0)
+        SPL("SPL",2),           // split (starts an execution thread at another address)
+        CMP("CMP",2),           // compare (same as SEQ)
+        SEQ("SEQ",2),           // skip if equal (compares two instructions, and skips the next instruction if they are equal)
+        SNE("SNE",2),           // skip if not equal (compares two instructions, and skips the next instruction if they aren't equal)
+        SLT("SLT",2),           // skip if lower than (compares two values, and skips the next instruction if the first is lower than the second)
+        LDP("LDP",2),           // load from p-space (loads a number from private storage space)
+        STP("STP",2),           // save to p-space (saves a number to private storage space)
+        NOP("NOP",0);           // no operation (does nothing)
 
         int argNum;
+        String command;
 
-        Instruction(int argNum)
+        Instruction(String command, int argNum)
         {
             this.argNum = argNum;
+            this.command = command;
         }
 
         public int getArgNum()
         {
             return argNum;
+        }
+
+        public String getCommand()
+        {
+            return command;
         }
     }
 
--- src/Genome.java
+++ src/Genome.java
@@ -1,7 +1,10 @@
+import java.util.List;
+
 /**
  * Created by c on 2/23/2017.
  */
 public class Genome
 {
+    private List<Gene> genome;
 
 }
