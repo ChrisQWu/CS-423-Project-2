6d1f476951afa6396780625d503d3d9011a728f4
jdonckels <jdonckels@unm.edu>
2017-03-08 12:48:07 -0700

    Merge branch 'master' of /nfs/student/j/jdonckels/IdeaProjects/CS-423-Project-2 with conflicts.

--- src/Main.java
+++ src/Main.java
@@ -178,9 +178,8 @@ public class Main
         for (int j = 0; j < numberIsland; j++)
         {
             ArrayList<Genome> temp = new ArrayList<>();
-            for (int i = 0; i < population_size/numberIsland; i++) {
-                temp.add(allWarriors.remove(0));
-            }
+            temp.addAll(allWarriors.subList(0,population_size/numberIsland-1));
+            allWarriors.removeAll(allWarriors.subList(0,population_size/numberIsland-1));
             islands[j].setCurrentPopulation(temp);
         }
         allWarriors.clear();
